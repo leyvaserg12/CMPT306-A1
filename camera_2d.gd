8be3e02a3a575b4be00b5bfe79f0de24af417301
Serg <leyvaserg12@gmail.com>
2024-09-19 14:27:11 -0500

    Step 12: Completed. Added randomized rotation of the obstacles when spawned, and rotating camera

---
+++ camera_2d.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+class_name Camera
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	#rotate the camera
+	self.rotation += delta*.35
--- obstacle.gd
+++ obstacle.gd
@@ -1,14 +1,21 @@
 extends AnimatableBody2D
 class_name Obstacle
 
-@export var shrink_speed := 5.0
+@export var shrink_speed := 2.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CollisionPolygon2D.scale = Vector2.ONE*10.0
 	
+	# randomize the rotation of the obstacle
+	$CollisionPolygon2D.rotate(randi_range(0,360))
+	
 func _physics_process(delta: float) -> void:
+	
 	$CollisionPolygon2D.scale -= Vector2.ONE * shrink_speed*delta
 	
 	#once tiny, delete
 	if $CollisionPolygon2D.scale.x < 0.5:
 		self.queue_free()
+	
+	
+		
